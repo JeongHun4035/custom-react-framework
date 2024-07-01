24b7ed2361cc1ce8f1e5c15330166d2c3c833b88
채정훈 <wjdgns4035@iabacus.co.kr>
2024-07-01 15:50:25 +0900

    eslint & prettier

--- eslint.config.js
+++ eslint.config.js
@@ -1,11 +1,12 @@
-import typescriptParser from "@typescript-eslint/parser"
 import typescriptPlugin from "@typescript-eslint/eslint-plugin"
+import typescriptParser from "@typescript-eslint/parser"
+import eslintCommentsPlugin from "eslint-plugin-eslint-comments"
+import importPlugin from "eslint-plugin-import"
+import pluginJsxA11y from "eslint-plugin-jsx-a11y"
 import reactPlugin from "eslint-plugin-react"
 import reactHooksPlugin from "eslint-plugin-react-hooks"
 import reactRefresh from "eslint-plugin-react-refresh"
-import pluginJsxA11y from "eslint-plugin-jsx-a11y"
 import unusedImportsPlugin from "eslint-plugin-unused-imports"
-import eslintCommentsPlugin from "eslint-plugin-eslint-comments"
 
 export default [
   // global
@@ -61,15 +62,50 @@ export default [
       ...pluginJsxA11y.configs.recommended.rules,
     },
   },
+  // others
   {
-    files: ["**/*.{js,jsx,mjs,cjs,ts,tsx}"],
     plugins: {
+      import: importPlugin,
       "eslint-comments": eslintCommentsPlugin,
       "unused-imports": unusedImportsPlugin,
     },
     rules: {
       "eslint-comments/no-unused-disable": "error",
       "unused-imports/no-unused-imports": "warn",
+      "import/order": [
+        "error",
+        {
+          groups: [
+            ["builtin", "external"], // Node.js built-in 모듈 및 외부 라이브러리
+            ["internal"], // 내부 모듈 (프로젝트 내에서 alias로 설정된 모듈 등)
+            ["parent", "sibling", "index"], // 부모, 형제, 인덱스 파일
+            ["type"], // 타입 관련 import
+            ["unknown"], // 알 수 없는 그룹 (기타)
+          ],
+          pathGroups: [
+            {
+              pattern: "react",
+              group: "builtin",
+              position: "before",
+            },
+            {
+              pattern: "@/**",
+              group: "internal",
+            },
+            {
+              pattern: "**/*.css",
+              group: "unknown",
+              position: "after",
+            },
+          ],
+          pathGroupsExcludedImportTypes: ["builtin"],
+          "newlines-between": "always",
+          alphabetize: {
+            order: "asc",
+            caseInsensitive: true,
+          },
+        },
+      ],
     },
   },
 ]
