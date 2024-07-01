9a33d6c79ea2f107e6153a3624612d7aa26ea217
채정훈 <wjdgns4035@iabacus.co.kr>
2024-07-01 16:29:31 +0900

    router

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,11 +1,11 @@
-import { createBrowserRouter } from "react-router-dom"
+import { createBrowserRouter, CustomRouteObject } from "react-router-dom"
 import Home from "@/pages/Home"
 import About from "@/pages/About"
 
 // Lazy Loading으로 초기 로딩 성능을 향상 가능
 // const About = lazy(() => import("@/pages/About"))
 
-const routes = [
+const routes: CustomRouteObject[] = [
   {
     path: "/",
     element: <Home />,
