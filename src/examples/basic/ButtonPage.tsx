190ccd321c691e95473d2f1d48c9a087e49eb009
채정훈 <wjdgns4035@iabacus.co.kr>
2024-07-08 16:28:32 +0900

    button example work

--- src/examples/basic/ButtonPage.tsx
+++ src/examples/basic/ButtonPage.tsx
@@ -1,5 +1,21 @@
+import CustomButton from "@/components/CustomButton"
+
 const ButtonPage = () => {
-  return <div>ButtonPage</div>
+  return (
+    <div>
+      <CustomButton name="normal" />
+      <CustomButton $primary name="primary" />
+      <CustomButton
+        name="custom"
+        $padding="20px"
+        $border="1px solid black"
+        $color="white"
+        $background="black"
+        $hoverColor="black"
+        $hoverBackground="white"
+      />
+    </div>
+  )
 }
 
 export default ButtonPage
